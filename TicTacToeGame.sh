ad33109dc6cca99bd4a4f687c71c2c404af590b6
janhavi07 <partejanhavi@gmail.com>
2019-11-18 13:19:02 +0530

    Added code to toss which one will start the game

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -5,6 +5,8 @@ echo "WELCOME TO TIC-TAC-TOE GAME"
 declare TOTAL_POSITIONS=9
 declare PLAYER_LETTER=X
 declare COMPUTER_LETTER=0
+declare HEADTOSS=1
+declare TAILTOSS=0
 
 #DICTIONARY DECLARATION
 declare -A boardChart
@@ -26,7 +28,20 @@ function toDisplayBoard()
 	echo "|" ${boardChart[1]} "|" ${boardChart[2]} "|" ${boardChart[3]} "|"
 
 }
+
+function toToss()
+{
+	headOrTail=$((RANDOM%2))
+	if [[ $headOrTail -eq $HEADTOSS ]]
+	then
+		echo "PLAYER STARTS THE GAME"
+	else
+		echo "COMPUTER STARTS THE GAME"
+	fi
+}
+
 echo "RESETING THE BOARD"
 toPrintTheResetBoard
 echo "PLAYER LETTER is: " $PLAYER_LETTER
 echo "COMPUTER LETTER is: " $COMPUTER_LETTER
+toToss
