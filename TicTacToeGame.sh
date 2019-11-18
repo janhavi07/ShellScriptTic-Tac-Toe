a301a67ad992dc471492fab2cd526d831d2eee34
janhavi07 <partejanhavi@gmail.com>
2019-11-18 12:50:50 +0530

    Adding code to print the board

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,3 +1,28 @@
-#!/bin/bash -x
+ #!/bin/bash -x
 echo "WELCOME TO TIC-TAC-TOE GAME"
 
+#VARIABLES
+declare TOTAL_POSITIONS=9
+
+#DICTIONARY DECLARATION
+declare -A boardChart
+
+
+function toPrintTheResetBoard()
+{
+	for(( position=1; position<=$TOTAL_POSITIONS; position++ ))
+	do
+		boardChart[$position]="_"
+	done
+	toDisplayBoard
+}
+
+function toDisplayBoard()
+{
+	echo "|" ${boardChart[1]} "|" ${boardChart[2]} "|" ${boardChart[3]} "|"
+	echo "|" ${boardChart[1]} "|" ${boardChart[2]} "|" ${boardChart[3]} "|"
+	echo "|" ${boardChart[1]} "|" ${boardChart[2]} "|" ${boardChart[3]} "|"
+
+}
+echo "RESETING THE BOARD"
+toPrintTheResetBoard
